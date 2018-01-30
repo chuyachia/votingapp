dc88eac2f6d51b9eb2a79b2fc3a30c57698281c8
Glitch (votingapp-chuya) <none>
2018-01-30 02:56:58 +0000

    👟👦 bundle and minified

--- app/controllers/plotPoll.client.js
+++ app/controllers/plotPoll.client.js
@@ -1,4 +1,8 @@
 'use strict';
+var $ = require('jquery');
+window.jQuery = $;
+require('bootstrap');
+require('chart.js');
 
 $(function()
 {
@@ -28,21 +32,24 @@ var chart = new Chart(ctx, {
         }]
     },
     options: {
-		tooltips: {
-			callbacks: {
-				label: function(tooltipItem, data) {
-					var allData = data.datasets[tooltipItem.datasetIndex].data;
-					var tooltipLabel = data.labels[tooltipItem.index];
-					var tooltipData = allData[tooltipItem.index];
-					var total = 0;
-					for (var i in allData) {
-						total += allData[i];
-					}
-					var tooltipPercentage = Math.round((tooltipData / total) * 100);
-					return tooltipLabel + ': ' + tooltipData + ' (' + tooltipPercentage + '%)';
-				}
-			}
-		}
+      legend:{
+       
+      },
+      tooltips: {
+        callbacks: {
+          label: function(tooltipItem, data) {
+            var allData = data.datasets[tooltipItem.datasetIndex].data;
+            var tooltipLabel = data.labels[tooltipItem.index];
+            var tooltipData = allData[tooltipItem.index];
+            var total = 0;
+            for (var i in allData) {
+              total += allData[i];
+            }
+            var tooltipPercentage = Math.round((tooltipData / total) * 100);
+            return tooltipLabel + ': ' + tooltipData + ' (' + tooltipPercentage + '%)';
+          }
+        }
+      }
 	}
 });
   
